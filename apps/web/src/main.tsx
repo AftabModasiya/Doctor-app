1dbeed7b26876cc898a4d5eca500070260f44523
aftabmodasiya123 <aftab.m@peerbits.com>
2026-03-05 18:55:28 +0530

    feat: add vercel analytics

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 			<ThemeProvider>
 				<AuthProvider>
 					<App />
+					<Analytics />
 					<Toaster
 						position="top-right"
 						toastOptions={{
