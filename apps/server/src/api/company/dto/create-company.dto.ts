2f02dd8cbdb9f523df52a06cf4d8c025e90287db
Azamkhan <72258523+AzamkhanPathan007@users.noreply.github.com>
2026-03-07 15:33:42 +0530

    Setup project pipeline and apply linting fixes (#5)
    * Linting fixes, extra library removed
    
    * feat:Pipeline setup for the project
    
    * Folder structure fixes for backend
    
    * feat:Docker fixes + zod integration

---
+++ apps/server/src/api/company/dto/create-company.dto.ts
@@ -0,0 +1,12 @@
+import { createZodDto } from "nestjs-zod";
+import { z } from "zod";
+
+export const CreateCompanySchema = z.object({
+	name: z.string().trim().min(1).max(255),
+	address: z.string().trim().min(1),
+	latitude: z.coerce.number().min(-90).max(90).optional(),
+	longitude: z.coerce.number().min(-180).max(180).optional(),
+	logo: z.url().optional(),
+});
+
+export class CreateCompanyDto extends createZodDto(CreateCompanySchema) {}
---
+++ apps/server/src/api/medicine/medicine.controller.ts
@@ -0,0 +1,45 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Patch,
+	Post,
+} from "@nestjs/common";
+import type { CreateMedicineDto } from "./dto/create-medicine.dto";
+import type { UpdateMedicineDto } from "./dto/update-medicine.dto";
+import { MedicineService } from "./medicine.service";
+
+@Controller("medicine")
+export class MedicineController {
+	constructor(private readonly medicineService: MedicineService) {}
+
+	@Post()
+	create(@Body() createMedicineDto: CreateMedicineDto) {
+		return this.medicineService.create(createMedicineDto);
+	}
+
+	@Get()
+	findAll() {
+		return this.medicineService.findAll();
+	}
+
+	@Get(":id")
+	findOne(@Param("id") id: string) {
+		return this.medicineService.findOne(+id);
+	}
+
+	@Patch(":id")
+	update(
+		@Param("id") id: string,
+		@Body() updateMedicineDto: UpdateMedicineDto,
+	) {
+		return this.medicineService.update(+id, updateMedicineDto);
+	}
+
+	@Delete(":id")
+	remove(@Param("id") id: string) {
+		return this.medicineService.remove(+id);
+	}
+}
